19b34c0dda605980ee9342c8eb84b7bfad38731b
mhmsdfhwhegggggggg <mhmmdfhd266@gmail.com>
2026-04-13 20:44:18 +0300

    رفع: lib/db/src/schema/index.ts

---
+++ lib/db/src/schema/index.ts
@@ -0,0 +1,7 @@
+export * from "./registrations";
+export * from "./news";
+export * from "./partners";
+export * from "./team";
+export * from "./stats";
+export * from "./slides";
+export * from "./contact-info";
